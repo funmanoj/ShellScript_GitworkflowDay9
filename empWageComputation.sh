ca0bf5f483ebb2e8a460356fb30ac388c3c11767
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 10:20:09 +0530

    Use case -7

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,23 @@
 #! /bin/bash -x
 
+
+get_work_hrs()
+{
+   emp_attendance=$1
+   case $emp_attendance in
+     0)
+       no_of_hrs_per_day=0
+       ;;
+     1)
+       no_of_hrs_per_day=8
+       ;;
+     2)
+       no_of_hrs_per_day=4
+       ;;
+   esac
+   echo "$no_of_hrs_per_day"
+
+}
 wage_per_hr=20
 max_hrs=100
 max_days=20
@@ -10,22 +28,8 @@ while [[ $total_working_hrs -lt $max_hrs && total_working_days -lt max_days ]]
 do
    ((total_working_days++))
    emp_attendance=$((RANDOM%3))
-   case $emp_attendance in
-     0)
-       echo "Employee is Absent"
-       no_of_hrs_per_day=0
-       ;;
-     1)
-       echo "Employee is full time present"
-       no_of_hrs_per_day=8
-       ;;
-     2)
-       echo "Employee is part time present"
-       no_of_hrs_per_day=4
-       ;;
-   esac
-
-       ((total_working_hrs+=no_of_hrs_per_day))
+   no_of_hrs_per_day=$( get_work_hrs $emp_attendance)
+   ((total_working_hrs+=no_of_hrs_per_day))
 done
 total_salary=$((total_working_hrs*wage_per_hr))
 echo " Total salary is $total_salary"
