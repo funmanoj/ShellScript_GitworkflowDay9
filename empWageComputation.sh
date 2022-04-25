e57c75c5f9fe71a41e658872963c2d3ed2e1bbe7
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 10:38:19 +0530

    Use case -9

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -24,7 +24,7 @@ max_days=20
 total_working_hrs=0
 total_working_days=0
 total_wage=0
-echo "daily wage    total wage"
+echo "Day  Daily wage   Total wage"
 while [[ $total_working_hrs -lt $max_hrs && total_working_days -lt max_days ]]
 do
    ((total_working_days++))
@@ -34,6 +34,6 @@ do
    daily_wage=$((no_of_hrs_per_day*wage_per_hr))
    ((total_wage+=daily_wage))
    wage[$total_wage]=$daily_wage
-   echo "$daily_wage    $total_wage"
+   echo "Day${total_working days}     $daily_wage    $total_wage"
 done
 
