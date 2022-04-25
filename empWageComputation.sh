d6f95796982e3bcd05eea8819552f39af48fd3c3
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 09:45:58 +0530

    Use case -4

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,20 @@
 #! /bin/bash -x
 
 attendance=$((RANDOM%3))
-if [ $attendance -eq 0 ]
-then
-   echo "Employee is Absent"
-   no_of_hrs_per_day=0
-elif [ $attendance -eq 1 ]
-then
-   echo "Employee is full time present"
-   no_of_hrs_per_day=8
-else
-   echo "Employee is part time present"
-   no_of_hrs_per_day=4
-fi
+case $attendance in
+     0)
+       echo "Employee is Absent"
+       no_of_hrs_per_day=0
+       ;;
+     1)
+       echo "Employee is full time present"
+       no_of_hrs_per_day=8
+       ;;
+     2)
+       echo "Employee is part time present"
+       no_of_hrs_per_day=4
+       ;;
+esac
 
 wage_per_hour=20
 wage_per_day=$((wage_per_hour*no_of_hrs_per_day))
