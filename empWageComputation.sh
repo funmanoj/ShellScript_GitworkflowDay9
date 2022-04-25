0ba9a046ff346db2df8aa0838471434778cc0da2
Manoj Funde <funde.manoj@gmail.com>
2022-04-25 10:17:39 +0530

    [Manoj] add use case-2

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,4 +8,9 @@ else
    echo "Employee is Present"
 fi
 
+# calculation of daily employee wage
+
+wage_per_hour=20
+no_of_hours=8
+echo " The daily Employee wage is $((attendance*wage_per_hour*no_of_hours))"
 
